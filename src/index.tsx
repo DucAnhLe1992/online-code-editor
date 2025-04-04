d6a09eb547d947ba9aa691bb2fcfe627f2bf1ee4
Duc Anh Le <gerald.duc.anh.le@gmail.com>
2025-04-05 00:04:34 +0300

    working on adding redux to the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 
 import CodeCell from "./components/code-cell";
 import TextEditor from "./components/text-editor";
+import { store } from "./state";
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-      {/* <CodeCell /> */}
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+        {/* <CodeCell /> */}
+      </div>
+    </Provider>
   );
 };
 
