43addaa96e93f596dd17ef14ad3cab5eabc58142
Duc Anh Le <gerald.duc.anh.le@gmail.com>
2025-04-08 02:35:35 +0300

    getting all the ports connected throughout packages

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,5 +1,16 @@
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+
 export const serve = (port: number, filename: string, dir: string) => {
-  console.log("Serving traffic on port ", port);
-  console.log("saving/fetching cells from ", filename);
-  console.log("that file is in dir ", dir);
+  const app = express();
+
+  app.use(createProxyMiddleware({
+    target: 'http://127.0.0.1:3000',
+    ws: true,
+    logLevel: 'silent',
+  }))
+
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on('error', reject);
+  });
 };
